a1df4e4b5cedbeddbbdcde79634856e2b2ab83c0
조용원 <auau3@kakao.com>
2025-03-12 15:15:59 +0900

    feature: LoginForm form action 추가 - 백엔드 로그인 API 기능 구현 완료 시 연동 로직 구현

--- containers/forms/LoginForm.tsx
+++ containers/forms/LoginForm.tsx
@@ -1,14 +1,28 @@
+"use client";
+
 import React from "react";
 import LoginInput from "@/components/form/login/LoginInput";
 import LoginButton from "@/components/form/login/LoginButton";
+import { toast, Toaster } from "react-hot-toast";
+import Form from "next/form";
 
 const LoginForm = () => {
   return (
-    <form className="flex h-auto w-auto flex-col items-start justify-start gap-2">
-      <LoginInput type="text" placeholder="ID" />
-      <LoginInput type="password" placeholder="PW" />
-      <LoginButton disabled={false} />
-    </form>
+    <>
+      <Form
+        action={(formData: FormData) => {
+          // TODO: 백엔드 로그인 API 기능 구현 완료 시 로직 구현
+          console.log(formData.get("dongle-id"));
+          toast.error("에러");
+        }}
+        className="flex h-auto w-auto flex-col items-start justify-start gap-2"
+      >
+        <LoginInput name="dongle-id" type="text" placeholder="ID" />
+        <LoginInput name="dongle-pw" type="password" placeholder="PW" />
+        <LoginButton disabled={false} />
+      </Form>
+      <Toaster />
+    </>
   );
 };
 
