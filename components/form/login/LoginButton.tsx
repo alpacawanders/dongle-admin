b9345e5c11aef5771515637ff86e87aa124d8926
조용원 <auau3@kakao.com>
2025-03-12 15:21:48 +0900

    fix: LoginButton cursor pointer 설정

--- components/form/login/LoginButton.tsx
+++ components/form/login/LoginButton.tsx
@@ -8,7 +8,7 @@ const LoginButton = ({ disabled }: Props) => {
   return (
     <button
       type="submit"
-      className="bg-primary-500 flex h-[30px] w-[140px] items-center justify-center rounded-md text-[12px] font-bold text-white"
+      className="bg-primary-500 flex h-[30px] w-[140px] cursor-pointer items-center justify-center rounded-md text-[12px] font-bold text-white"
       disabled={disabled}
     >
       로그인
