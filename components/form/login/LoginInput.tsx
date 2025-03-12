e967160f7d481354f07173bf8ec9351e1a5824e0
조용원 <auau3@kakao.com>
2025-03-12 15:15:15 +0900

    fix: LoginInput name attribute 추가

--- components/form/login/LoginInput.tsx
+++ components/form/login/LoginInput.tsx
@@ -2,13 +2,15 @@ import React from "react";
 
 interface Props {
   type: "text" | "password";
+  name: string;
   placeholder: string;
 }
 
-const LoginInput = ({ type, placeholder }: Props) => {
+const LoginInput = ({ type, name, placeholder }: Props) => {
   return (
     <input
       type={type}
+      name={name}
       className="outline-primary-500 h-[30px] w-[140px] rounded-md border border-zinc-200 p-2 text-[12px]"
       placeholder={placeholder}
     />
