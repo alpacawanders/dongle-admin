0824079afc5e6ecca61092ba2f59907f7d803195
조용원 <auau3@kakao.com>
2025-03-12 15:15:28 +0900

    fix: LoginButton type submit 설정

--- components/form/login/LoginButton.tsx
+++ components/form/login/LoginButton.tsx
@@ -7,6 +7,7 @@ interface Props {
 const LoginButton = ({ disabled }: Props) => {
   return (
     <button
+      type="submit"
       className="bg-primary-500 flex h-[30px] w-[140px] items-center justify-center rounded-md text-[12px] font-bold text-white"
       disabled={disabled}
     >
