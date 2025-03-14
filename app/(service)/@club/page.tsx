f1a465bce491cab3b409642854f9daa8238fdeaf
조용원 <auau3@kakao.com>
2025-03-14 10:47:08 +0900

    feature: 메인화면 NoticeList 컴포넌트 추가

--- app/(service)/@club/page.tsx
+++ app/(service)/@club/page.tsx
@@ -1,5 +1,4 @@
 import BannerSlide from "@/components/common/BannerSlide";
-import FlexBox from "@/containers/common/FlexBox";
 import ShortcutButton from "@/components/common/ShortcutButton";
 import NoticeList from "@/containers/common/NoticeList";
 
@@ -7,16 +6,10 @@ export default function Page() {
   return (
     <>
       <BannerSlide />
-      <FlexBox
-        width="full"
-        direction="row"
-        justify="between"
-        align="center"
-        gap={4}
-      >
+      <div className="flex h-auto w-full flex-row items-center justify-between gap-4">
         <ShortcutButton text="동아리 정보 관리" href="/clubs" />
         <ShortcutButton text="활동보고서 관리" href="/reports" />
-      </FlexBox>
+      </div>
       <NoticeList />
     </>
   );
---
+++ components/icons/NewLabel.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const NewLabel = () => {
+  return (
+    <svg
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="18" height="18" rx="4" fill="#FB2C36" />
+      <path
+        d="M12.375 13H10.5059L7.35938 8.42383H7.30078V13H5.07422V4.51562H6.97852L10.0781 9.08008H10.1484V4.51562H12.375V13Z"
+        fill="white"
+      />
+    </svg>
+  );
+};
+
+export default NewLabel;
